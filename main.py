3207be8e59dba08ec7c09dbc93fcfcb5126bd95e
Prince Rios <riosprince7@gmail.com>
2020-05-15 13:17:38 -0700

    edited main and shop pages

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, flash, request, session, current_app
 from PIL import Image
 app = Flask(__name__)
 
@@ -7,21 +7,22 @@ products = [
 	{
 		'id': 't-shirt.jpg',
 		'product_name': 'Under6 T-Shirt',
-		'price': '50.00 USD',
-		'buy_now': 'Add to cart'
+		'price': '50.00 USD'
 	}
 ]
 
 lst = []
 
-@app.route('/')
-def home():
-  return render_template('template.html')
+# @app.route('/')
+# def home():
+#   return render_template('template.html')
 
 @app.route('/shop')
 def shop():
 	return render_template('template2.html', dict = products)
-
+@app.route('/addCart')
+def addCart():
+	redirect()
 @app.route('/cart')
 def cart():
 	return render_template('template3.html')
